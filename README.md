dfec4e645a2128a164eac970851c39095be597bd
Hong Viet Le <hvle@octo.com>
2022-11-27 19:48:33 +0100

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+## URLs
+- http://localhost:8080/
+- http://localhost:3000/
+- http://localhost:9090/
+
+## References
+- https://opentelemetry.io/blog/2022/instrument-nginx/
+- https://github.com/grafana/tempo/tree/main/example/docker-compose/local
+- 
---
+++ jspring/src/main/java/com/octo/jspring/JspringApplication.java
@@ -0,0 +1,13 @@
+package com.octo.jspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspringApplication.class, args);
+	}
+
+}
